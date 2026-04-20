9910fc7cb6ebe7702e54af10b2398d0e0557d0f9
Kaushik Chowdhury <kaushik.tsx@gmail.com>
2026-04-20 21:48:28 +0530

    feat: add billing service and grpc

---
+++ billing-service/src/main/java/org/pm/billingservice/grpc/BillingServiceGrpc.java
@@ -0,0 +1,28 @@
+package org.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingServiceGrpc extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpc.class);
+
+    @Override
+    public void createBllingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("Received billing request for {}", request.toString());
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("ACC-6493574198")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
